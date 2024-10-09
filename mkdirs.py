c5449e6eab4a6e6f36e35d0caa4ab4715b70af75
Christian Nielsen <nielsenchristianole@gmail.com>
2024-10-09 08:40:18 +0200

    renamed weeks to worksheets

--- mkdirs.py
+++ mkdirs.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
-for idx_week, num_parts in enumerate((5, 4, 4, 6, 4, 4, 4, 4, 6, 4), start=1):
+for idx_worksheet, num_parts in enumerate((5, 4, 4, 6, 4, 4, 4, 4, 6, 4), start=1):
 
     for idx_part in range(1, 1+num_parts):
-        _dir = Path(f"./exercises/week{idx_week:0>1}/part{idx_part}")
+        _dir = Path(f"./exercises/worksheet{idx_worksheet:0>1}/part{idx_part}")
         _dir.mkdir(parents=True, exist_ok=True)
