11b93d5a4cfa8bba16107c1699dc6618db19a2a8
Christian Ole Nielsen <nielsenchristianole@gmail.com>
2024-10-30 08:04:19 +0100

    cleaned code

--- worksheets/worksheet6/part3/script.js
+++ worksheets/worksheet6/part3/script.js
@@ -1,14 +1,5 @@
 "use strict"
 
-
-/**
- * @param {Element} canvas. The canvas element to create a context from.
- * @return {WebGLRenderingContext} The created context.
- */
-function setupWebGL(canvas) {
-    return WebGLUtils.setupWebGL(canvas)
-}
-
 /** @type {WebGLRenderingContext} */
 var gl
 var canvas
@@ -32,89 +23,6 @@ var ambientRange = [1.0, 0.0, 1.0]
 var diffuseRange = [0.0, 0.0, 1.0]
 var specularRange = [0.0, 0.0, 1.0]
 var shineRange = [500.0, 0.0000000000001, 1000.0]
-// var redRange = [1.0, 0.0, 1.0]
-// var greenRange = [1.0, 0.0, 1.0]
-// var blueRange = [1.0, 0.0, 1.0]
-
-
-function mix(a, b) {
-    return scale(0.5, add(a, b))
-}
-
-function divideTriangle(a, b, c, count, pointsArray, is_face) {
-    if (count > 0) {
-        var ab = normalize(mix(a, b), true)
-        var ac = normalize(mix(a, c), true)
-        var bc = normalize(mix(b, c), true)
-        divideTriangle(a, ab, ac, count - 1, pointsArray, is_face)
-        divideTriangle(ab, b, bc, count - 1, pointsArray, is_face)
-        divideTriangle(bc, c, ac, count - 1, pointsArray, is_face)
-        divideTriangle(ab, bc, ac, count - 1, pointsArray, is_face)
-    } else {
-        if (is_face) {
-            pointsArray.push(a)
-            pointsArray.push(b)
-            pointsArray.push(c)
-        } else {
-            pointsArray.push(a)
-            pointsArray.push(b)
-            pointsArray.push(b)
-            pointsArray.push(c)
-            pointsArray.push(c)
-            pointsArray.push(a)
-        }
-    }
-}
-
-function tetrahedron(count, is_face=true) {
-    var pointsArray = []
-    
-    var a = vec4(0.0, 0.0, 1.0, 1)
-    var b = vec4(0.0, 0.942809, -0.333333, 1)
-    var c = vec4(-0.816497, -0.471405, -0.333333, 1)
-    var d = vec4(0.816497, -0.471405, -0.333333, 1)
-
-    divideTriangle(a, b, c, count, pointsArray, is_face)
-    divideTriangle(d, c, b, count, pointsArray, is_face)
-    divideTriangle(a, d, b, count, pointsArray, is_face)
-    divideTriangle(a, c, d, count, pointsArray, is_face)
-    return pointsArray
-}
-
-
-async function load_texture(filename)
-{
-    var texture = gl.createTexture()
-    gl.bindTexture(gl.TEXTURE_2D, texture)
-
-    const pixel = new Uint8Array([255, 0, 255, 255])
-    gl.texImage2D(
-        gl.TEXTURE_2D, 
-        0,
-        gl.RGBA,
-        1,
-        1,
-        0,
-        gl.RGBA,
-        gl.UNSIGNED_BYTE,
-        pixel
-    )
-
-    var image = document.createElement('img')
-    image.crossorigin = 'anonymous'
-    image.onload = function () { 
-
-        gl.bindTexture(gl.TEXTURE_2D, texture)
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image)
-        gl.generateMipmap(gl.TEXTURE_2D)
-
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT)
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT)
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST)
-    }
-    image.src = filename
-}
 
 
 window.onload = async function init() {
@@ -158,14 +66,12 @@ window.onload = async function init() {
         "change",
         function() {
             wrappingMode = this.value
-            // render()
         })
 
     document.getElementById("filtering-select").addEventListener(
         "change",
         function() {
             filteringMode = this.value
-            // render()
         })
 
     // UI update
@@ -174,18 +80,12 @@ window.onload = async function init() {
     document.getElementById("diffuseRange").value = Math.round((diffuseRange[0] - diffuseRange[1]) / diffuseRange[2] * 1000)
     document.getElementById("specularRange").value = Math.round((specularRange[0] - specularRange[1]) / specularRange[2] * 1000)
     document.getElementById("shineRange").value = Math.round((shineRange[0] - shineRange[1]) / shineRange[2] * 1000)
-    // document.getElementById("redRange").value = Math.round((redRange[0] - redRange[1]) / redRange[2] * 1000)
-    // document.getElementById("greenRange").value = Math.round((greenRange[0] - greenRange[1]) / greenRange[2] * 1000)
-    // document.getElementById("blueRange").value = Math.round((blueRange[0] - blueRange[1]) / blueRange[2] * 1000)
 
     document.getElementById('emittedDisplay').innerHTML = emittedRange[0]
     document.getElementById('ambientDisplay').innerHTML = ambientRange[0]
     document.getElementById('diffuseDisplay').innerHTML = diffuseRange[0]
     document.getElementById('specularDisplay').innerHTML = specularRange[0]
     document.getElementById('shineDisplay').innerHTML = shineRange[0]
-    // document.getElementById('redDisplay').innerHTML = redRange[0]
-    // document.getElementById('greenDisplay').innerHTML = greenRange[0]
-    // document.getElementById('blueDisplay').innerHTML = blueRange[0]
     
 
     document.getElementById('TUI').innerHTML = 'Using ' + NumSubdivisions + ' subdivisions'
@@ -246,24 +146,6 @@ window.onload = async function init() {
         function() {
             shineRange[0] = Math.max(this.value / 1000 * shineRange[2], shineRange[1])
             document.getElementById('shineDisplay').innerHTML = shineRange[0]})
-    
-    // document.getElementById("redRange").addEventListener(
-    //     "input",
-    //     function() {
-    //         redRange[0] = Math.max(this.value / 1000 * redRange[2], redRange[1])
-    //         document.getElementById('redDisplay').innerHTML = redRange[0]})
-
-    // document.getElementById("greenRange").addEventListener(
-    //     "input",
-    //     function() {
-    //         greenRange[0] = Math.max(this.value / 1000 * greenRange[2], greenRange[1])
-    //         document.getElementById('greenDisplay').innerHTML = greenRange[0]})
-
-    // document.getElementById("blueRange").addEventListener(
-    //     "input",
-    //     function() {
-    //         blueRange[0] = Math.max(this.value / 1000 * blueRange[2], blueRange[1])
-    //         document.getElementById('blueDisplay').innerHTML = blueRange[0]})
 
     render()
 }
@@ -297,10 +179,6 @@ function render() {
             diffuseRange[0],
             specularRange[0],
             shineRange[0]])
-    
-    // gl.uniform3fv(
-    //     gl.getUniformLocation(program, "bodyColor"), flatten(vec3(redRange[0], greenRange[0], blueRange[0]))
-    // )
 
     // render
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
@@ -355,4 +233,84 @@ function render() {
 
     gl.drawElements(gl.TRIANGLES, vertices.length, gl.UNSIGNED_INT, 0)
     requestAnimationFrame(render)
+}
+
+
+/**
+ * @param {Element} canvas. The canvas element to create a context from.
+ * @return {WebGLRenderingContext} The created context.
+ */
+function setupWebGL(canvas) {
+    return WebGLUtils.setupWebGL(canvas)
+}
+
+
+function mix(a, b) {
+    return scale(0.5, add(a, b))
+}
+
+
+function divideTriangle(a, b, c, count, pointsArray, is_face) {
+    if (count > 0) {
+        var ab = normalize(mix(a, b), true)
+        var ac = normalize(mix(a, c), true)
+        var bc = normalize(mix(b, c), true)
+        divideTriangle(a, ab, ac, count - 1, pointsArray, is_face)
+        divideTriangle(ab, b, bc, count - 1, pointsArray, is_face)
+        divideTriangle(bc, c, ac, count - 1, pointsArray, is_face)
+        divideTriangle(ab, bc, ac, count - 1, pointsArray, is_face)
+    } else {
+        if (is_face) {
+            pointsArray.push(a)
+            pointsArray.push(b)
+            pointsArray.push(c)
+        } else {
+            pointsArray.push(a)
+            pointsArray.push(b)
+            pointsArray.push(b)
+            pointsArray.push(c)
+            pointsArray.push(c)
+            pointsArray.push(a)
+        }
+    }
+}
+
+
+function tetrahedron(count, is_face=true) {
+    var pointsArray = []
+    
+    var a = vec4(0.0, 0.0, 1.0, 1)
+    var b = vec4(0.0, 0.942809, -0.333333, 1)
+    var c = vec4(-0.816497, -0.471405, -0.333333, 1)
+    var d = vec4(0.816497, -0.471405, -0.333333, 1)
+
+    divideTriangle(a, b, c, count, pointsArray, is_face)
+    divideTriangle(d, c, b, count, pointsArray, is_face)
+    divideTriangle(a, d, b, count, pointsArray, is_face)
+    divideTriangle(a, c, d, count, pointsArray, is_face)
+    return pointsArray
+}
+
+
+async function load_texture(filename) {
+    var texture = gl.createTexture()
+    gl.bindTexture(gl.TEXTURE_2D, texture)
+
+    const pixel = new Uint8Array([255, 0, 255, 255])
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, pixel)
+
+    var image = document.createElement('img')
+    image.crossorigin = 'anonymous'
+    image.onload = function () { 
+
+        gl.bindTexture(gl.TEXTURE_2D, texture)
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image)
+        gl.generateMipmap(gl.TEXTURE_2D)
+
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT)
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT)
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST)
+    }
+    image.src = filename
 }
